78f89b4090065167034cb4a65b1f338ac3441b0e
TheCoderCrab <54598040+TheCoderCrab@users.noreply.github.com>
2021-04-14 11:52:50 +0000

    Added some help commands

--- sbs_dir/verbs_builtins.bash
+++ sbs_dir/verbs_builtins.bash
@@ -1,14 +1,40 @@
 #!/bin/bash
 
+SBS_INSTALLATION="${HOME}/.local/share/sbs/"
+
+source ${SBS_INSTALLATION}/colors.bash
+source ${SBS_INSTALLATION}/term.bash
+
+help() {
+    print ${bold}${fg_green} "Welcome to sbs version ${fg_yellow}$(cat ${SBS_INSTALLATION}/sbs.version)${fg_green}!"
+    print ${bold}${fg_green} "SBS is a build system for C/C++/Assembly projects"
+    print ${bold}${fg_green} "Currently it is preferable to use GNU C/C++ compilers and NASM to assemble"
+    print ""
+    print ${bold}${fg_green} "Documentation for SBS is still in progress"
+    print ""
+    print ${bold}${fg_green}  "Those are the builtin verbs: "
+    print ${bold}${fg_yellow} "\thelp${reset}${fg_green}      : " "Display this menu"
+    print ${bold}${fg_yellow} "\tversion${reset}${fg_green}   : " "Display installed SBS version"
+    print ${bold}${fg_yellow} "\tupdate${reset}${fg_green}    : " "Check if an update is available and install it"
+    print ${bold}${fg_yellow} "\tnew${reset}${fg_green}       : " "Create a new project basic layout"
+    print ${bold}${fg_yellow} "\tproj-setup${reset}${fg_green}: " "Interactive menu to configure the project"
+    print ${bold}${fg_yellow} "\tbuild${reset}${fg_green}     : " "Build the project using the configuration"
+    print ${bold}${fg_yellow} "\tclean${reset}${fg_green}     : " "Clean builds"
+}
+
+version() {
+    print ${bold}${fg_green} "SBS version ${fg_yellow}$(cat ${SBS_INSTALLATION}/sbs.version)"
+}
+
 new() {
     AUTO_SETUP=${2:-"no-setup-guide"}
-    cp ${HOME}/.local/share/sbs/default_sbs_verbs ./sbs.verbs
+    cp ${SBS_INSTALLATION}/default_sbs_verbs ./sbs.verbs
     if [[ ${AUTO_SETUP} = "setup-guide" ]]; then
         proj-setup
     elif [[ ${AUTO_SETUP} = "no-setup-guide" ]]; then
-        echo "You can launch project setup guide with 'sbs proj-setup'"
+        print ${bold}${fg_green} "You can launch project setup guide with 'sbs proj-setup'"
     else
-        echo "Unknown parameter: ${2}"
+        print ${bold}${fg_green} "Unknown parameter: ${2}"
         exit 1
     fi
     touch sbs.project
@@ -32,8 +58,8 @@ proj-setup() {
     read -p "Subprojects path                : " SUB_PROJECTS
     read -p "Libraries                       : " LIBRARIES
     read -p "Library directories             : " LIB_DIRS
-    echo "Writing configuration to sbs.project"
-    echo "# This file was auto generated, you may need to adjust it to your needs
+    print ${bold}${fg_green} "Writing configuration to sbs.project"
+    print ${bold}${fg_green} "# This file was auto generated, you may need to adjust it to your needs
 CC=${CC}
 CXX=${CXX}
 AS=${AS}
@@ -60,32 +86,32 @@ LIB_DIRS=${LIB_DIRS}" > sbs.project
 }
 
 build() {
-    source ${HOME}/.local/share/sbs/sbs.bash
+    source ${SBS_INSTALLATION}/sbs.bash
 }
 
 clean() {
-    echo "Cleaning objects and temporary files"
+    print ${bold}${fg_green} "Cleaning objects and temporary files"
     if [[ -f ${PROJECT_FILE} ]]; then
         source ${PROJECT_FILE}
     else
-        echo "Project file not found!"
+        print ${bold}${fg_green} "Project file not found!"
         exit 1
     fi
     BUILD_DIR=${BUILD_DIR:-"${PROJECT_DIR}/build/"}
     _RBDIR=${_RBDIR:-"${BUILD_DIR}/"}
     [[ -d "${_RBDIR}/debug/obj" ]] && rm -rf "${_RBDIR}/debug/obj"
     [[ -d "${_RBDIR}/release/obj" ]] && rm -rf "${_RBDIR}/release/obj"
-    [[ -d "${HOME}/.local/share/sbs/tmp" ]] && rm -rf "${HOME}/.local/share/sbs/tmp"
-    echo "Cleaned!"
+    [[ -d "${SBS_INSTALLATION}/tmp" ]] && rm -rf "${SBS_INSTALLATION}/tmp"
+    print ${bold}${fg_green} "Cleaned!"
 }
 
 remove_update_files() {
-    echo "Removing temporary file..."
+    print ${bold}${fg_green} "Removing temporary file..."
     cd ${PROJECT_DIR}
     rm -rf ${TMP_DIR}
     if [[ $? -ne 0 ]]; then
-        echo "Problem while trying to delete temporary files!"
-        echo "Try deleting '${TMP_DIR}' by yourself, it should be deleted automatically after reboot"
+        print ${bold}${fg_green} "Problem while trying to delete temporary files!"
+        print ${bold}${fg_green} "Try deleting '${TMP_DIR}' by yourself, it should be deleted automatically after reboot"
         exit 1
     fi
 }
@@ -95,45 +121,45 @@ update() {
     cd ${TMP_DIR}
 
     if [[ ! $(command -v jq) ]]; then
-        echo "Cannot find command jq, please install it first"
+        print ${bold}${fg_green} "Cannot find command jq, please install it first"
         exit 1
     fi
 
     GITHUB_API_LATEST="https://api.github.com/repos/TheCoderCrab/ShellBuildSystem/releases/latest"
 
-    echo "Fetching update meta-data from: \"${GITHUB_API_LATEST}\"..."
+    print ${bold}${fg_green} "Fetching update meta-data from: \"${GITHUB_API_LATEST}\"..."
 
     if [[ $(command -v wget) ]]; then
         TAG_NAME=$(wget -O - -q ${GITHUB_API_LATEST} | jq -r ".tag_name")
     elif [[ $(command -v curl) ]]; then
         TAG_NAME=$(curl -s ${GITHUB_API_LATEST} | jq -r ".tag_name")
     else
-        echo "Please install wget or curl before trying to update"
+        print ${bold}${fg_green} "Please install wget or curl before trying to update"
         cd ${PROJECT_DIR}
         remove_update_files
         exit 1
     fi
 
     
-    if [[ -f ${HOME}/.local/share/sbs/sbs.version ]]; then
-        CURRENT_VERSION=$(cat ${HOME}/.local/share/sbs/sbs.version)
+    if [[ -f ${SBS_INSTALLATION}/sbs.version ]]; then
+        CURRENT_VERSION=$(cat ${SBS_INSTALLATION}/sbs.version)
     else
         CURRENT_VERSION="unknown"
     fi
 
-    echo "Latest version: ${TAG_NAME}"
-    echo "Current version: ${CURRENT_VERSION:-"unknown"}"
+    print ${bold}${fg_green} "Latest version: ${TAG_NAME}"
+    print ${bold}${fg_green} "Current version: ${CURRENT_VERSION:-"unknown"}"
 
     if [[ ${CURRENT_VERSION} != ${TAG_NAME} ]]; then
-        echo "Installing new update..."
+        print ${bold}${fg_green} "Installing new update..."
     else
-        echo "Latest update already installed"
+        print ${bold}${fg_green} "Latest update already installed"
         exit 0
     fi
 
     UPDATE_URL="https://github.com/TheCoderCrab/ShellBuildSystem/archive/${TAG_NAME}.tar.gz"
     
-    echo "Downloading update..."
+    print ${bold}${fg_green} "Downloading update..."
     
     if [[ $(command -v wget) ]]; then
         CMD="wget -q"
@@ -142,44 +168,43 @@ update() {
     fi
     ${CMD} ${UPDATE_URL}
     if [[ $? -ne 0 ]]; then
-        echo "Problem while downloading the update files!"
+        print ${bold}${fg_green} "Problem while downloading the update files!"
         remove_update_files
         exit 1
     fi
-    echo "Extracting files..."
+    print ${bold}${fg_green} "Extracting files..."
     tar -xzf ${TAG_NAME}.tar.gz
     if [[ $? -ne 0 ]]; then
-        echo "Problem while extracting the update files!"
+        print ${bold}${fg_green} "Problem while extracting the update files!"
         remove_update_files
         exit 1
     fi
-    echo "Copying new files to sbs directory..."
-    \cp -r ShellBuildSystem*/sbs_dir/* ${HOME}/.local/share/sbs
+    print ${bold}${fg_green} "Copying new files to sbs directory..."
+    \cp -r ShellBuildSystem*/sbs_dir/* ${SBS_INSTALLATION}
     if [[ $? -ne 0 ]]; then
-        echo "Problem while copying the update files!"
+        print ${bold}${fg_green} "Problem while copying the update files!"
         remove_update_files
         exit 1
     fi
     sudo cp ShellBuildSystem*/sbs /usr/bin/sbs
     if [[ $? -ne 0 ]]; then
-        echo "Problem while copying the update files!"
-        echo "Perhaps try to run the command as super user"
+        print ${bold}${fg_green} "Problem while copying the update files!"
+        print ${bold}${fg_green} "Perhaps try to run the command as super user"
         remove_update_files
         exit 1
     fi
     cd ${PROJECT_DIR}
     remove_update_files
-    echo ${TAG_NAME} > ${HOME}/.local/share/sbs/sbs.version
-    echo "Updated sbs!"
+    print ${bold}${fg_green} "Updated sbs!"
 }
 
 delete() {
     TMP=$(mktemp)
-    echo "rm -rf ${HOME}/.local/share/sbs/
+    print ${bold}${fg_green} "rm -rf ${SBS_INSTALLATION}/
 sudo rm -rf /bin/sbs
 rm -rf ${TMP}" > ${TMP}
     source ${TMP}
 }
 
-NOT_VERB+=(remove_update_files)
+NOT_VERB+=(remove_update_files print debug warn error)
 
